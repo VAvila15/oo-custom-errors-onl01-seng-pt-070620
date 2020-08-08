ad030eeff7bcdedf3d1ed91a94d46a11961bb2dd
Victor Avila <avilavictor1570@gmail.com>
2020-08-08 22:03:57 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,12 +9,12 @@ class Person
     self.partner = person
     if person.class != Person
       begin
-      raise PartnerError
+        raise PartnerError
       rescue PartnerError => error
-      puts error.message
-    end
+          puts error.message
+      end
     else
-    person.partner = self
+      person.partner = self
     end
   end
   
